99ca6c025eaef79a96dc08e2065cd77ba39bea9c
mgfacioli <mgfacioli@yahoo.com.br>
2024-02-29 14:56:16 -0300

    Adicionando nova versão.

--- atualiza_sistema.sh
+++ atualiza_sistema.sh
@@ -29,12 +29,11 @@ dirMensal=""$mes"_"$n_mes$(date +%Y)""
 dia=$(date +%d)
 #dia=22
 time=$(date +%T)
-DELAY=10 # Number of seconds to display results
 
 
 # ====== funcoes do projeto ======
 function separador {
-    echo -e "================================================================================\n"
+    echo -e "\n================================================================================\n"
     return
 }
 
@@ -46,7 +45,17 @@ function cabec_abertura {
     separador
 }
 
-
+function rodape () {
+    if [ -z "$2" ]; then
+        DELAY=5
+    else
+        DELAY="$2"
+    fi
+    separador
+    echo -e "$1"
+    separador
+    sleep "$DELAY" 
+}
 
 # ====== main ======
 
@@ -55,69 +64,54 @@ while true; do
     cabec_abertura
 
 cat <<- _EOF_
-    Please Select:
-    1. Fazer Update
-    2. Quantidade de pacotes Atualizáveis
-    3. Listar Pacotes Atualizáveis
-    4. Fazer Upgrade
-    0. Quit
+    Selecione:
+    A. Fazer Update
+    B. Listar Pacotes Atualizáveis
+    C. Fazer Upgrade
+    Q. Quit
 
 _EOF_
-    read -p "Enter selection [0-3] > "
+    read -p "Opção [A, B, C, D ou Q] > "
 
-    if [[ "$REPLY" =~ ^[0-4]$ ]]; then
-        if [[ "$REPLY" == 1 ]]; then
+    case "$REPLY" in
+        q|Q) rodape "Programa encerrado!"
+            exit
+        ;;
+        a|A) rodape "Update já vai começar!!"
             sudo apt update
-            sleep "$DELAY"
-            continue
-        fi
-        if [[ "$REPLY" == 2 ]]; then
-            total=$(apt list --upgradable | wc -l)
-            echo "Qtd. total de pacotes atualizáveis: $total." 
-            sleep "$DELAY"
-            continue
-        fi        
-        if [[ "$REPLY" == 3 ]]; then
-            sudo apt list --upgradable | tee /media/mgfacioli/PortableSSD/Learning/Linux/Bash_scripts/super_update/log_atualizacao.txt
-            sleep "$DELAY"
-            continue
-        fi
-        if [[ "$REPLY" == 4 ]]; then
-            read -p "Iniciar o Upgrade? [s/n] > "
-            if [[ "$REPLY" == 's' ]]; then
-                echo "Upgrade já vai começar!!"
-                sleep "$DELAY"
-                sudo apt upgrade
-                sleep "$DELAY"
-                continue
-            elif [[ "$REPLY" == 'n' ]]; then
-                echo "Upgrade Cancelado!!"
-                sleep "$DELAY"
-                continue
-            else
-                echo "Entrada inválida."
-                sleep "$DELAY"
-            fi
-        fi
-        if [[ "$REPLY" == 0 ]]; then
-            break
-        fi
-    else
-        echo "Entrada inválida."
-        sleep "$DELAY"
-    fi
+            rodape
+        ;;
+        b|B) rodape "Verificando total de pacotes atualizáveis. Aguarde..."
+            total=$(apt list --upgradable | tail -n +2 | wc -l)
+            case "$total" in
+                0) rodape "Não há pacotes a serem atualizados."
+                ;;
+                *) rodape "Qtd. total de pacotes atualizáveis: $total." 10
+                    read -p "Deseja listar pacotes atualizáveis? [s/n] > "
+                    case "$REPLY" in
+                        s|S) sudo apt list --upgradable ## | tee /media/mgfacioli/PortableSSD/Learning/Linux/Bash_scripts/super_update/log_atualizacao.txt
+                        ;;
+                        n|N) rodape "Cancelando."
+                        ;;
+                        *) rodape "Entrada inválida. Digite s ou n."
+                        ;;
+                    esac
+                ;;
+            esac
+        ;;
+        c|C) read -p "Iniciar o Upgrade? [s/n] > "
+            case "$REPLY" in
+                s|S) rodape "Upgrade já vai começar!!"
+                    sudo apt upgrade
+                    rodape "Upgrade finalizado!!!"
+                ;;
+                n|N) rodape "Upgrade Cancelado!!"
+                ;;
+                *) rodape "Entrada inválida. Digite s ou n." 10
+                ;;
+            esac
+        ;;
+        *) rodape "Entrada inválida. Digite uma letra do menu..." 10
+        ;;
+    esac
 done
-echo "Atualização Encerrada."
-
-
-
-# echo "==============================" >> /home/mgfacioli/Documentos/processo.log
-# echo "Processo realizado em ${dia}/${mes}/${ano}, ${time}." >> /home/mgfacioli/Documentos/processo.log
-# echo "Verfificando atualizacoes disponiveis..."
-
-# apt list --upgradable
-
-# total=$(apt list --upgradable | wc -l)
-# echo "==================================" >> /home/mgfacioli/Documentos/processo.log
-# echo "Total de $total atualizacoes." >> /home/mgfacioli/Documentos/processo.log
-# echo "==================================" >> /home/mgfacioli/Documentos/processo.log
